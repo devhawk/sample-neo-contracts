b154e00b627224aed482ef03279e40a95b473eb6
Harry Pierson <harrypierson@hotmail.com>
2019-05-12 10:37:24 -0700

    adder contract

---
+++ adder/Adder.cs
@@ -0,0 +1,19 @@
+﻿using Neo.SmartContract.Framework;
+using System;
+using System.Numerics;
+using System.Reflection;
+
+[assembly: AssemblyDescription("Sample Smart Contract")]
+[assembly: AssemblyMetadata("ContractAuthor", "Harry Pierson")]
+[assembly: AssemblyMetadata("ContractEmail", "harrypierson@hotmail.com")]
+
+namespace DevHawk.Neo.Experiments
+{
+    public class Adder : SmartContract
+    {
+        public static BigInteger Main(BigInteger lhs, BigInteger rhs)
+        {
+            return lhs + rhs;
+        }
+    }
+}
