af1fea7a08d0af824386ab65ae306d5c0091d718
Harry Pierson <harrypierson@outlook.com>
2019-05-30 17:32:25 -0700

    add negator contract

---
+++ negator/Negator.cs
@@ -0,0 +1,17 @@
+﻿using Neo.SmartContract.Framework;
+using System.Reflection;
+
+[assembly: AssemblyDescription("Sample Smart Contract")]
+[assembly: AssemblyMetadata("ContractAuthor", "Harry Pierson")]
+[assembly: AssemblyMetadata("ContractEmail", "harrypierson@hotmail.com")]
+
+namespace NegatorContract
+{
+    public class Negator : SmartContract
+    {
+        public static bool Main(bool value)
+        {
+            return !value;
+        }
+    }
+}
